133cb85955c8abbd085bf9c93f16e3995caa223e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-07 23:06:15 +0100

    modify Program.cs add exercise to iterate inserted data

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Globalization;
 
 namespace FirstProject
+
+    /* Zadanie:
+
+    Utwórz aplikację konsolową, która będzie wczytywać wartości liczbowe od użytkownika, dopóki nie poda on wartości 0.
+
+    Podczas czytania, aplikacja powinna:
+
+    obliczyć sumę wszystkich elementów
+
+    znaleźć największy element
+
+    Po wczytaniu wartości 0, te dwie wartości powinny zostać wypisane w konsoli. */
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert numbers: ");
+
+            int chosenNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int? highestNumber = null;
+
+            while (chosenNumber != 0)
+            {
+                if (chosenNumber == null || chosenNumber > highestNumber)
+                {
+                    highestNumber = chosenNumber;
+                }
+                sum += chosenNumber;
+                chosenNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Sum of numbers = {sum}");
+            if (highestNumber != null)
+            {
+            Console.WriteLine($"The highest number is: {highestNumber}");
+
+            } else
+            {
+                Console.WriteLine("No values inserted");
+            }
         }
     }
 }
