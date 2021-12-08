ae89818712b38e9c255a047a7c1da76c30d3dd3e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-08 11:50:45 +0100

    delete Gender.cs file modify FirstProject.cs add example of parsing dates

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,24 +1,35 @@
-﻿namespace FirstProject
+﻿using System;
+using System.Globalization;
+
+namespace FirstProject
 {
     class Program
     {
+
+        /* Utwórz aplikację konsolową która pobierze
+         * od użytkownika datę jego urodzin i na tej podstawie
+         * obliczy ile mięło dni od tej daty do obecnego dnia. */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your gender? 1 - Male, 2 - Female");
-            string userInput = Console.ReadLine();
+            /* Console.WriteLine("Insert the day");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the month");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the year");
+            int year = int.Parse(Console.ReadLine());
 
-            Gender userGender = (Gender)Enum.Parse(typeof(Gender), userInput);
+            DateTime dateOfBirth = new DateTime(year, month, day);
 
-            if (userGender == Gender.Male)
-            {
-                Console.WriteLine("Only women are allowed");
-            }
-            else
-            {
-                Console.WriteLine("Hi");
-            }
+            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
+            Console.WriteLine($"You were born {timeSpan.TotalDays} days ago"); */
 
-            Console.ReadLine();
+            Console.WriteLine("Insert the date of birth (dd.MM.yyyy)");
+            string dateOfBirthString = Console.ReadLine();
+            DateTime dateOfBirth = DateTime.Parse(dateOfBirthString);
+            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
+            Console.WriteLine($"You were born: {timeSpan.TotalDays} days ago");
+
 
         }
     }
