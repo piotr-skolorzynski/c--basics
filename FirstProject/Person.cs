69af91a95aa2725a7ba298822f9a460c6ebb3e9d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-09 14:36:21 +0100

    create class Person, modify Program.css add example of using setter and class methods

---
+++ FirstProject/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        private DateTime dateOfBirth;
+
+        public void SetDateOfBirth(DateTime date)
+        {
+            if(date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date of birth");
+            }
+            else
+            {
+                dateOfBirth = date;
+            }
+        }
+
+        public DateTime GetDateOfBirth()
+        {
+            return dateOfBirth;
+        }
+        // można zastosować expression value 
+        // public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {FirstName} {LastName} and I was born {GetDateOfBirth()}");
+        }
+    }
+
+}
+
+
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -5,32 +5,12 @@ namespace FirstProject
 {
     class Program
     {
-
-        /* Utwórz aplikację konsolową która pobierze
-         * od użytkownika datę jego urodzin i na tej podstawie
-         * obliczy ile mięło dni od tej daty do obecnego dnia. */
-
         static void Main(string[] args)
         {
-            /* Console.WriteLine("Insert the day");
-            int day = int.Parse(Console.ReadLine());
-            Console.WriteLine("Insert the month");
-            int month = int.Parse(Console.ReadLine());
-            Console.WriteLine("Insert the year");
-            int year = int.Parse(Console.ReadLine());
-
-            DateTime dateOfBirth = new DateTime(year, month, day);
-
-            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
-            Console.WriteLine($"You were born {timeSpan.TotalDays} days ago"); */
-
-            Console.WriteLine("Insert the date of birth (dd.MM.yyyy)");
-            string dateOfBirthString = Console.ReadLine();
-            DateTime dateOfBirth = DateTime.Parse(dateOfBirthString);
-            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
-            Console.WriteLine($"You were born: {timeSpan.TotalDays} days ago");
-
-
+            Person bill = new Person();
+            bill.FirstName = "Bill";
+            bill.SetDateOfBirth(new DateTime(1982, 8, 25));
+            bill.SayHi();
         }
     }
 }
