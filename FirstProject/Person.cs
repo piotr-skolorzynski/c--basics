0a1e10cc11ebb31b429d372a8e0272b180a76102
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-09 16:25:56 +0100

    modify class Person add examples of constructor method modify Person.cs file add example of using created constructors

--- FirstProject/Person.cs
+++ FirstProject/Person.cs
@@ -11,6 +11,27 @@ namespace FirstProject
 
         private DateTime dateOfBirth;
 
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        /*public Person(DateTime dateOfBirth, string firstName, string lastName)
+        {
+            SetDateOfBirth(dateOfBirth);
+            FirstName = firstName;
+            LastName=lastName;
+        }*/
+        //zapisa powyższego konstruktora z 3 parametrami można uprościć, a bardziej zaznaczyć, że najpierw
+        // ma się wykonać konstruktor z 2 parametrami a następnie trzema
+
+        public Person(DateTime dateOfBirth, string firstName, string lastName) : this(firstName, lastName)
+        {
+            SetDateOfBirth(dateOfBirth);
+        }
+
+        // konstruktorów można mieć wiele, byle różniły się między sobą m.in. liczbą i typami parametrów
         public void SetDateOfBirth(DateTime date)
         {
             if(date > DateTime.Now)
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,10 +7,12 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            Person bill = new Person();
-            bill.FirstName = "Bill";
+            Person bill = new Person("Bill", "Wick");
             bill.SetDateOfBirth(new DateTime(1982, 8, 25));
             bill.SayHi();
+
+            Person john = new Person(new DateTime(1990, 2, 2), "John", "Malcowich");
+            john.SayHi();
         }
     }
 }
