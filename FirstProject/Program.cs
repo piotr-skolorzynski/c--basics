05057811512dc840858b2da652040b8236ac6171
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-11 22:45:29 +0100

    delete ExcelFile File PowerPointFile Shape and WordDocumentFile Modify Program.cs Add example of List collection and its methods

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,15 +7,35 @@ namespace FirstProject
 {
     class Program
     {
+        //method to display List
+        static void DisplayElements(List<int> list)
+        {
+            Console.WriteLine("** List **");
+            foreach (int element in list)
+            {
+                Console.Write($"{element}, ");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            // example of polymorphism
-            Shape[] shapes = { new Circle(), new Rectangle(), new Triangle() };
+           List<int> intList = new List<int>() { 6, 1, 20, 3, 45, 60, 100, 2 }; //creating collection type List with default values
+            DisplayElements(intList); //displaying declared List
+            //adding element to the List
+            Console.WriteLine("New element: ");
+            int newElement = int.Parse(Console.ReadLine());
+            intList.Add(newElement);//adding new element to the list
+            DisplayElements(intList); //display List again to show that new element was added to the List
 
-            foreach (Shape shape in shapes)
-            {
-                shape.Draw();
-            }
+            //example of using method RemoveRange
+            Console.WriteLine("Remove range");
+            intList.RemoveRange(1, 2);
+            DisplayElements(intList);
+
+            //sorting example
+            Console.WriteLine("Sort");
+            intList.Sort();
+            DisplayElements(intList);
         }
     }
 }
