f374317282c38014cef955cee61836eef263b43c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-12 22:53:24 +0100

    modify Program.cs refactor code using dictionary type collection

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -8,26 +8,26 @@ namespace FirstProject
 {
     class Program
     {
-        static List<Currency> GetCurrencies()
+        static Dictionary<string,Currency> GetCurrencies()
         {
-            return new List<Currency>
+            return new Dictionary<string, Currency>
             {
-                new Currency("usd", "United States Dollar", 1),
-                new Currency("eur", "Euro", 0.83975),
-                new Currency("gbp", "British Pound", 0.74771),
-                new Currency("cad", "Canadian Dollar", 1.30724),
-                new Currency("inr", "Indian Rupee", 73.04),
-                new Currency("mxn", "Mexican Peso", 21.7571),
+                {"usd", new Currency("usd", "United States Dollar", 1) },
+                {"eur", new Currency("eur", "Euro", 0.83975) },
+                {"gbp", new Currency("gbp", "British Pound", 0.74771) },
+                {"cad", new Currency("cad", "Canadian Dollar", 1.30724) },
+                {"inr", new Currency("inr", "Indian Rupee", 73.04) },
+                {"mxn", new Currency("mxn", "Mexican Peso", 21.7571) },
             };
         }
         static void Main(string[] args)
         {
-            List<Currency> currencies = GetCurrencies();
+            Dictionary<string, Currency> currencies = GetCurrencies();
             Console.WriteLine("Check the rate for:");
             string userInput  = Console.ReadLine();
 
-            Currency selectedCurrency = currencies.FirstOrDefault(c => c.Name == userInput);
-            if (selectedCurrency != null)
+            Currency selectedCurrency = null;
+            if (currencies.TryGetValue(userInput, out selectedCurrency))
             {
                 Console.WriteLine($"Rate for USD to {selectedCurrency.FullName} is {selectedCurrency.Rate}");
             }
@@ -35,6 +35,10 @@ namespace FirstProject
             {
                 Console.WriteLine("Currency not found");
             }
+
+            currencies.Remove("usd"); //how to delete element in dictionary
+
+            currencies.Add("usd", new Currency("pln", "Polski złoty", 4.45)); //adding element to dictionary
         }
     }
 }
