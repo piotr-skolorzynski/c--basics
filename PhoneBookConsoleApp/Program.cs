7da8501006a321ac2a698ac1f5e902a1e2ed9af9
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-13 22:36:22 +0100

    modify Program.cs add basic information with options to choose create switch to react on chosen operation

--- PhoneBookConsoleApp/Program.cs
+++ PhoneBookConsoleApp/Program.cs
@@ -7,6 +7,29 @@ namespace PhoneBookConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from PhoneBook app ;)");
+            Console.WriteLine("Possible actions: ");
+            Console.WriteLine("1 Add contact");
+            Console.WriteLine("2 Display contact by number");
+            Console.WriteLine("3 Display all contacts");
+            Console.WriteLine("4 Search contacts");
+
+            var userInput = Console.ReadLine();
+
+            //react on chosen option
+            switch (userInput)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
         }
     }
 }
