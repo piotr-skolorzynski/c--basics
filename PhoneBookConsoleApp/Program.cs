e81d7a3f1705c5d35415754a94b02366ae388112
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-13 19:46:12 +0100

    create new project PhoneBookConsoleApp

---
+++ PhoneBookConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneBookConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from PhoneBook app ;)");
+        }
+    }
+}
