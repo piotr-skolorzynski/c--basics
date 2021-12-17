21182e234cd7167ca31654bfd5181711f2c4e0ae
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-17 13:54:07 +0100

    create directory Read move document1 and document2 to Read directory Create directory Write in folder TextFiles create template text file Modify Program.cs add method GenerateDocuments as example how to save text files on hard drive using template from prepared text file

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -12,8 +12,8 @@ namespace FirstProject
         static void ReadFiles()
         {
             //reading text files
-            var document1 = File.ReadAllText(@"D:\csharp-basics\FirstProject\TextFiles\document1.txt");
-            var document2 = File.ReadAllLines(@"D:\csharp-basics\FirstProject\TextFiles\document2.txt");
+            var document1 = File.ReadAllText(@"D:\csharp-basics\FirstProject\TextFiles\Read\document1.txt");
+            var document2 = File.ReadAllLines(@"D:\csharp-basics\FirstProject\TextFiles\Read\document2.txt");
 
             Console.WriteLine("document1:");
             Console.WriteLine(document1);
@@ -24,13 +24,33 @@ namespace FirstProject
                 Console.WriteLine(element);
             }*/
             //second solution
-            var documentToString =  string.Join(Environment.NewLine, document2);
+            var documentToString = string.Join(Environment.NewLine, document2);
             Console.WriteLine(documentToString);
+        }
+
+        //example of saving text file using template in template.txt file
+        static void GenerateDocuments()
+        {
+            Console.WriteLine("Insert name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Insert order number");
+            var orderNumber = Console.ReadLine();
+
+            var template = File.ReadAllText(@"D:\csharp-basics\FirstProject\TextFiles\Write\template.txt");
+            //replace values in template
+            var document = template.Replace("{name}", name)
+                .Replace("{orderNumber}", orderNumber)
+                .Replace("{dateTime}", DateTime.Now.ToString());
+            //create txt file on hard drive
+            File.WriteAllText($"D:/csharp-basics/FirstProject/TextFiles/Write/document-{name}.txt", document);
+
         }
         static void Main(string[] args)
         {
             //execute method
             ReadFiles();
+            GenerateDocuments();
         }
     }
 }
