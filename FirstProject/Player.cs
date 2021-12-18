f81c76db92a6384d31da6322f2d662b3bf284fa8
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-18 19:55:27 +0100

    add Newtonsoft.Json package to enable serialization of object to JSON files Modify FirstProject directory Create Player and Statistics classes Create Directory JSON in folder TextFiles Modify Program.cs Add example how to serialize and deserialize objects

---
+++ FirstProject/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class Player
+    {
+        public string Name { get; set; }    
+        public int Level { get; set; }
+        public int HealPoints { get; set; }
+        public List<Statistics> Statistics { get; set; }
+
+    }
+}
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -4,64 +4,47 @@ using System.Text;
 using System.Globalization;
 using System.Linq;
 using System.IO;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace FirstProject
 {
     class Program
     {
-        static void ReadFiles()
-        {
-            //reading text files
-            var document1 = File.ReadAllText(@"D:\csharp-basics\FirstProject\TextFiles\Read\document1.txt");
-            var document2 = File.ReadAllLines(@"D:\csharp-basics\FirstProject\TextFiles\Read\document2.txt");
-
-            Console.WriteLine("document1:");
-            Console.WriteLine(document1);
-            Console.WriteLine("document2:");
-            //first solution to show text from table
-            /*foreach (var element in document2)
-            {
-                Console.WriteLine(element);
-            }*/
-            //second solution
-            var documentToString = string.Join(Environment.NewLine, document2);
-            Console.WriteLine(documentToString);
-        }
-
-        //example of saving text file using template in template.txt file
-        static void GenerateDocuments()
-        {
-            Console.WriteLine("Insert name:");
-            var name = Console.ReadLine();
-
-            Console.WriteLine("Insert order number");
-            var orderNumber = Console.ReadLine();
-
-            var template = File.ReadAllText(@"D:\csharp-basics\FirstProject\TextFiles\Write\template.txt");
-            //replace values in template
-            var document = template.Replace("{name}", name)
-                .Replace("{orderNumber}", orderNumber)
-                .Replace("{dateTime}", DateTime.Now.ToString());
-            //create txt file on hard drive
-            File.WriteAllText($"D:/csharp-basics/FirstProject/TextFiles/Write/document-{name}.txt", document);
-        }
-
-        static void ScanAndAppend()
-        {
-            //getting name of files and directories
-            var files = Directory.GetFiles("D:/csharp-basics/FirstProject/TextFiles/Append/", "*.txt", SearchOption.AllDirectories);
-            //adding text to all selected files
-            foreach (var file in files)
-            {
-                File.AppendAllText(file, "All rights reserved");
-            }
-        }
         static void Main(string[] args)
         {
-            //execute method
-            //ReadFiles();
-            //GenerateDocuments();
-            ScanAndAppend();
+            /*var player = new Player()
+            {
+                Name = "Mario",
+                Level = 1,
+                HealPoints = 100,
+                Statistics = new List<Statistics>()
+                {
+                    new Statistics()
+                    {
+                        Name = "Strength",
+                        Points = 10
+                    },
+                    new Statistics()
+                    {
+                        Name = "Intelligence",
+                        Points = 10
+                    }
+                }
+            };
+
+            //..
+            player.Level++;
+
+            //serialize object to JSON
+            string playerSerialized = JsonConvert.SerializeObject(player);
+            //save serialized object in file
+            File.WriteAllText(@"D:/csharp-basics/FirstProject/TextFiles/JSON/playerSerialized.json", playerSerialized);*/
+
+            //deserialization
+            string playerSerialized = File.ReadAllText(@"D:/csharp-basics/FirstProject/TextFiles/JSON/playerSerialized.json");
+            Player player = JsonConvert.DeserializeObject<Player>(playerSerialized);
+            Console.WriteLine(player.Name);
         }
     }
 }
