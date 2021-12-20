5c7c0f80442e144f5378ebba689db4052257d9f2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-20 21:15:55 +0100

    modify Program.cs in FirstProject directory add examps of using selected method on values type string

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -13,38 +13,70 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            /*var player = new Player()
+            Console.WriteLine("Insert input:");
+            string userInput = Console.ReadLine();
+
+            //SubString(userInput);
+            //Replace(userInput);
+            //Modify(userInput);
+            //Split(userInput);
+            // CheckString(userInput);
+        }
+
+        //examp of using method Substring
+        static void SubString(string userInput)
+        {
+            if(userInput.Length > 10)
             {
-                Name = "Mario",
-                Level = 1,
-                HealPoints = 100,
-                Statistics = new List<Statistics>()
-                {
-                    new Statistics()
-                    {
-                        Name = "Strength",
-                        Points = 10
-                    },
-                    new Statistics()
-                    {
-                        Name = "Intelligence",
-                        Points = 10
-                    }
-                }
-            };
+                string startSubstring = userInput.Substring(0,10);
+                string endSubstring = userInput.Substring(userInput.Length - 10);
+                Console.WriteLine($"{startSubstring}..., ...{endSubstring}");
 
-            //..
-            player.Level++;
+            }
+        }
 
-            //serialize object to JSON
-            string playerSerialized = JsonConvert.SerializeObject(player);
-            //save serialized object in file
-            File.WriteAllText(@"D:/csharp-basics/FirstProject/TextFiles/JSON/playerSerialized.json", playerSerialized);*/
+        static void Replace(string userInput)
+        {
+            string template = "Hello {name}, how are you?";
+            string output = template.Replace("{name}", userInput);
+            Console.WriteLine(output);
+        }
 
-            //deserialization
-            string playerSerialized = File.ReadAllText(@"D:/csharp-basics/FirstProject/TextFiles/JSON/playerSerialized.json");
-            Player player = JsonConvert.DeserializeObject<Player>(playerSerialized);
-            Console.WriteLine(player.Name);
+        static void Modify(string userInput)
+        {
+            string removedString = userInput.Remove(5);
+            string insertedString = userInput.Insert(5, "INSERT");
+            Console.WriteLine(insertedString);
+            string trimmedString = userInput.Trim();
+            Console.WriteLine(trimmedString);
+
+        }
+
+        static void AlterTextCase(string userInput)
+        {
+            string upperCased = userInput.ToUpper();
+            string lowerCased = userInput.ToLower();
+
+            Console.WriteLine(upperCased);
+            Console.WriteLine(lowerCased);
+        }
+
+        static void Split(string userInput)
+        {
+            string[] inputParts = userInput.Split(" ");
+            string firstName = inputParts[0];
+            string lastName = inputParts[inputParts.Length - 1];
+            Console.WriteLine($"Hello {firstName} {lastName}");
+        }
+
+        static void CheckString(string userInput)
+        {
+            bool isTextFile = userInput.EndsWith(".txt", StringComparison.CurrentCultureIgnoreCase);
+            bool startsWithDocPrefix = userInput.StartsWith("doc-", StringComparison.CurrentCultureIgnoreCase);
+            bool containsText = userInput.Contains("TEXT", StringComparison.CurrentCultureIgnoreCase);
+            Console.WriteLine($"containsText: {containsText}");
+            Console.WriteLine($"isTextFile: {isTextFile}");
+            Console.WriteLine($"startsWithDocPrefix: {startsWithDocPrefix}");
         }
     }
 }
