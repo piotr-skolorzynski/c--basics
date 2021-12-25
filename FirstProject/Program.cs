b1a5a9d588cf2146cadfa7a9dd161f0bdaeae714
piotr-skolorzynski <p.skolorzynski@gmail.com>
2021-12-25 23:51:21 +0100

    create Address and Person classes Modify Program.cs Add example how to use reflection in displaying objects porperties

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Web;
+using System.Reflection;
 using Newtonsoft.Json;
 
 
@@ -12,64 +13,42 @@ namespace FirstProject
 {
     class Program
     {
-        //creating HTTP client using json placeholders
-        static async Task OldMain(string[] args)
+        //using reflection
+        static void Display(object obj)
         {
-            using(var httpClient = new HttpClient()) 
+            Type objType = obj.GetType();
+            var properties = objType.GetProperties();
+            foreach (var property in properties)
             {
-                var result = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
-                var json = await result.Content.ReadAsStringAsync();
-                //checking what is in json variable
-                //Console.WriteLine(json);
-
-                //deserialize json data to selected type
-                var posts = JsonConvert.DeserializeObject<List<Post>>(json);
-
-                //choose selected post and show its properties
-                var selectedPost = posts.First(p => p.Id == 30);
-                Console.WriteLine(selectedPost.Title);
-                Console.WriteLine(selectedPost.Body);
-
-                //change property value of selected
-                selectedPost.Title = "test tile";
-                selectedPost.Body = "test body";
-                Console.WriteLine(selectedPost.Title);
-                Console.WriteLine(selectedPost.Body);
-
-                //creating new post and sending to server
-                var postJsonContent = new StringContent(JsonConvert.SerializeObject(selectedPost)); //createing body for our Post request
-                var postResult = await httpClient.PostAsync("https://jsonplaceholder.typicode.com/posts", postJsonContent); //sending POST request to server
-
-
-                //other way to create http request
-                /*using (var postRequestMessage = 
-                    new HttpRequestMessage(HttpMethod.Put, "https://jsonplaceholder.typicode.com/posts"))
+                var propValue = property.GetValue(obj);
+                //check value type 
+                var propType = propValue.GetType();
+                if(propType.IsPrimitive || propType == typeof(string))
                 {
-                    //adding exemplary request header, you can add as many as you need
-                    postRequestMessage.Headers.Add("content-type", "application/json");
-                    postRequestMessage.Headers.Add("someheader", "somevalue");
-
-                    //adding Request's body
-                    postRequestMessage.Content = postJsonContent; 
-
-                    //sending request to server
-                    var post2Result = httpClient.SendAsync(postRequestMessage);
-                }*/
-
-                //creating request parameters
-                var queryParams = HttpUtility.ParseQueryString(string.Empty);
-                //create params
-                queryParams["postId"] = "1";
-                queryParams["someParam"] = "someValue";
-                //format params
-                var formattedParams = queryParams.ToString();
-
-                //see paramas
-                Console.WriteLine(formattedParams);
-
+                    Console.WriteLine($"{property.Name}: {propValue}");
+                }
             }
         }
+        static void Main(string[] args)
+        {
+            Address address = new Address()
+            {
+                City = "Krakow",
+                PostalCode = "31-556",
+                Street = "Grodzka 5"
+            };
 
+            Person person = new Person()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Address = address
+            };
+            Console.WriteLine("Person:");
+            Display(person);
+            Console.WriteLine("Address:");
+            Display(address);
+        }
     }
 }
 
